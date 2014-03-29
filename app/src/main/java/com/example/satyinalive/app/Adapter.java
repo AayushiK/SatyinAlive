0776b2decbb9e030c4e80918072fcfe6806bd688
Aayushi Kaushik <aayushikaushik@gmail.com>
2014-03-29 14:13:16 -0700

    Added new activity that is launched when you click on someone's face

--- app/src/main/java/com/example/satyinalive/app/Adapter.java
+++ app/src/main/java/com/example/satyinalive/app/Adapter.java
@@ -1,6 +1,8 @@
 package com.example.satyinalive.app;
 
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
@@ -47,6 +49,13 @@ public class Adapter extends BaseAdapter{
         image.setImageBitmap(BitmapFactory.decodeResource(
                 mContext.getResources(),
                 R.drawable.ic_launcher));
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(mContext, ContactActivity.class);
+                mContext.startActivity(mainIntent);
+            }
+        });
         //set onClickListener for image here to launch a new fragment.
         return image;
     }
---
+++ app/src/main/java/com/example/satyinalive/app/ContactActivity.java
@@ -0,0 +1,20 @@
+package com.example.satyinalive.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aayushi on 3/29/14.
+ */
+public class ContactActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
