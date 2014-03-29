cea365a877ee2f5d011e9692985f5c47db5f6d7e
Aayushi Kaushik <aayushikaushik@gmail.com>
2014-03-29 13:07:36 -0700

    Push

--- app/src/main/java/com/example/satyinalive/app/MainActivity.java
+++ app/src/main/java/com/example/satyinalive/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.satyinalive.app;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("rawr", "workin");
     }
 
 
