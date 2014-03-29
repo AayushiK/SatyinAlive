66190742972eed05789cc1e8d42a380bcd7e0492
schan <sarah.peiqui.chan@gmail.com>
2014-03-29 13:39:00 -0700

    Added User Profile Activity

--- app/src/main/java/com/example/satyinalive/app/ProfileActivity.java
+++ app/src/main/java/com/example/satyinalive/app/ProfileActivity.java
@@ -1,16 +1,22 @@
 package com.example.satyinalive.app;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
+
+    private ImageView mProfileImage;
+    private TextView mUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        mProfileImage = (ImageView) findViewById(R.id.profile_image);
+        mUsername = (TextView) findViewById(R.id.username);
     }
 
 }
