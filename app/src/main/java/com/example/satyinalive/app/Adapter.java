42e575b6eea3fc421d72936e9f8a4e4f65e4bb4b
Aayushi Kaushik <aayushikaushik@gmail.com>
2014-03-29 13:27:19 -0700

    Added images to grid view

--- app/src/main/java/com/example/satyinalive/app/Adapter.java
+++ app/src/main/java/com/example/satyinalive/app/Adapter.java
@@ -1,9 +1,14 @@
 package com.example.satyinalive.app;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by aayushi on 3/29/14.
@@ -11,28 +16,38 @@ import android.widget.BaseAdapter;
 public class Adapter extends BaseAdapter{
 
     private Context mContext;
+    private List<Contact> mContacts;
 
     public Adapter(Context context) {
         mContext = context;
     }
 
+    public void setContacts(List<Contact> contacts) {
+        mContacts = contacts;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return mContacts.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return mContacts.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        ImageView image = new ImageView(mContext);
+        image.setImageBitmap(BitmapFactory.decodeResource(
+                mContext.getResources(),
+                R.drawable.ic_launcher));
+        //set onClickListener for image here to launch a new fragment.
+        return image;
     }
 }
